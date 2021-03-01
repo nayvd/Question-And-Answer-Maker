1aca9f63761a81b8701cb24412345acc770c07b6
Nayif <76570869+pythonprogrammerandjs@users.noreply.github.com>
2021-03-01 00:08:31 -0600

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-question_chooser = input('Choose a question: ')
-answer_chooser = input('Choose a answer: ')
-
-question = input(question_chooser)
-if question == answer_chooser:
-    print('Correct!')
-
-else:
-    print('Incorrect the answer was ' + answer_chooser + '!')
