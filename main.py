4dd797eda58e303c1d6336274025ba5d5f8cb277
Nayif <76570869+pythonprogrammerandjs@users.noreply.github.com>
2021-03-01 00:09:51 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+question_chooser = input('Choose a question: ')
+answer_chooser = input('Choose a answer: ')
+
+question = input(question_chooser)
+if question == answer_chooser:
+    print('Correct!')
+
+else:
+    print('Incorrect the answer was ' + answer_chooser + '!')
+    
+print('Enjoyed this project? Well thanks! Follow me on GitHub Link: [https://github.com/pythonprogrammerandjs] Thanks!')
